b84ef67d10bf1f1af35dd92c523dade6057d58e7
Jia1213733590 <1213733590@qq.com>
2025-12-16 14:22:39 +0000

    create UserRepository and verify database integration

---
+++ backend/gym/src/main/java/com/example/gym/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.gym.repository;
+
+import com.example.gym.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
